38bb4274e3a2a3220cc11068fcc6ef9e6d990ebe
Emily Siow <emily.siow@digital.gov.au>
2015-12-07 12:11:30 +1100

    Set up server-side templating engine

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const express = require('express'),
+      consolidate = require('consolidate'),
       sassMiddleware = require('node-sass-middleware'),
       webpack = require('webpack'),
       webpackMiddleware = require('webpack-dev-middleware'),
       path = require('path'),
       app = express(),
       default_port = (process.env.PORT || 3000);
+
+app.set('view engine', 'mustache');
+app.engine('mustache', consolidate.hogan);
+app.set('views', path.join(__dirname, 'views', 'server'));
 
 app.use(sassMiddleware({
     src: path.join(__dirname, 'sass'),
@@ -29,7 +34,10 @@ app.use(webpackMiddleware(webpack(require('./webpack.config')), {
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(require('connect-livereload')());  // runs livereload server and serves livereload.js
 require('express-livereload')(app, { watchDir: path.join(__dirname) });  // inserts <script> reference to livereload.js
-app.use('/', express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 let server = app.listen(default_port, function () {
   let port = server.address().port;
