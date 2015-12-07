380c84f6a3bd06b6af45e6ba629b68d96a7bb227
Emily Siow <emily.siow@digital.gov.au>
2015-12-07 14:54:16 +1100

    Live reload of mustache templates
    Include mustache extension in expess-livereload watcher

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(sassMiddleware({
 app.use(webpackMiddleware(webpack(require('./webpack.config')), {
     noInfo: false,
     quiet: false,
-    lazy: true,
+    lazy: false,
     publicPath: "/",
     stats: {
         colors: true
@@ -34,7 +34,7 @@ app.use(webpackMiddleware(webpack(require('./webpack.config')), {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(require('connect-livereload')());  // runs livereload server and serves livereload.js
-require('express-livereload')(app, { watchDir: path.join(__dirname) });  // inserts <script> reference to livereload.js
+require('express-livereload')(app, { watchDir: path.join(__dirname), exts: ['mustache'] });  // inserts <script> reference to livereload.js
 
 app.get('/', (req, res) => {
   res.render('index', {
