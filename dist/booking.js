7b6485962200f13c4e46bb2823599eb69009c2b8
Emily Siow <emily.siow@digital.gov.au>
2016-01-06 11:02:18 +1100

    Format selected appointment ISO datetime string using Moment

--- dist/booking.js
+++ dist/booking.js
@@ -72,6 +72,8 @@
 
 	'use strict';
 	
+	var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; })();
+	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -341,7 +343,15 @@
 	
 	        self.settings.selectedTime = $(this).attr('data-time');
 	        var data = self.displayTime(self.settings.selectedTime);
-	        data['selected_appointment'] = self.settings.selectedDate + "T" + self.settings.selectedTime + ":00";
+	
+	        var _self$settings$select = self.settings.selectedTime.split(":");
+	
+	        var _self$settings$select2 = _slicedToArray(_self$settings$select, 2);
+	
+	        var hours = _self$settings$select2[0];
+	        var minutes = _self$settings$select2[1];
+	
+	        data['selected_appointment'] = moment.utc(self.settings.selectedDate).add({ hours: hours, minutes: minutes }).format('YYYY-MM-DDTHH:mm:SS');
 	        data['display_date'] = moment.utc(self.settings.selectedDate).format("dddd D MMMM");
 	        var output = selection_template.render(data);
 	        document.querySelector('tr.CalRow-selection[data-week=\'' + week + '\'] td').innerHTML = output;
