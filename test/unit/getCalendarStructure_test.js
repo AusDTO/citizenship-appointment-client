6ce5a03c40ec656f1e1bf779b2e9950222cbbfd4
Halvard Skogsrud <halvard@skogsrud.com>
2016-01-28 17:50:03 +1100

    Fix unit test

--- test/unit/getCalendarStructure_test.js
+++ test/unit/getCalendarStructure_test.js
@@ -33,265 +33,300 @@ test('get calendar structure', (assert) => {
           {
             "days": [
               {
-                date: "2015-11-30",
-                today: false,
-                day: 30,
-                available_times_count: 0,
-                bookable: false
+                "date": "2015-11-30",
+                "day": 30,
+                "ariaDay": "Monday 30th November",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": false
               },
               {
-                date: "2015-12-01",
-                today: false,
-                day: 1,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-01",
+                "day": 1,
+                "ariaDay": "Tuesday 1st December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-02",
-                today: false,
-                day: 2,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-02",
+                "day": 2,
+                "ariaDay": "Wednesday 2nd December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-03",
-                today: false,
-                day: 3,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-03",
+                "day": 3,
+                "ariaDay": "Thursday 3rd December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-04",
-                today: false,
-                day: 4,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-04",
+                "day": 4,
+                "ariaDay": "Friday 4th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-05",
-                today: false,
-                day: 5,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-05",
+                "day": 5,
+                "ariaDay": "Saturday 5th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-06",
-                today: false,
-                day: 6,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-06",
+                "day": 6,
+                "ariaDay": "Sunday 6th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               }
             ]
           },
           {
-            days: [
+            "days": [
               {
-                date: "2015-12-07",
-                today: false,
-                day: 7,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-07",
+                "day": 7,
+                "ariaDay": "Monday 7th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-08",
-                today: false,
-                day: 8,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-08",
+                "day": 8,
+                "ariaDay": "Tuesday 8th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-09",
-                today: false,
-                day: 9,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-09",
+                "day": 9,
+                "ariaDay": "Wednesday 9th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-10",
-                today: false,
-                day: 10,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-10",
+                "day": 10,
+                "ariaDay": "Thursday 10th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-11",
-                today: false,
-                day: 11,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-11",
+                "day": 11,
+                "ariaDay": "Friday 11th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-12",
-                today: true,
-                day: 12,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-12",
+                "day": 12,
+                "ariaDay": "Saturday 12th December",
+                "today": true,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-13",
-                today: false,
-                day: 13,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-13",
+                "day": 13,
+                "ariaDay": "Sunday 13th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               }
             ]
           },
           {
-            days: [
+            "days": [
               {
-                date: "2015-12-14",
-                today: false,
-                day: 14,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-14",
+                "day": 14,
+                "ariaDay": "Monday 14th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-15",
-                today: false,
-                day: 15,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-15",
+                "day": 15,
+                "ariaDay": "Tuesday 15th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-16",
-                today: false,
-                day: 16,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-16",
+                "day": 16,
+                "ariaDay": "Wednesday 16th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-17",
-                today: false,
-                day: 17,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-17",
+                "day": 17,
+                "ariaDay": "Thursday 17th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-18",
-                today: false,
-                day: 18,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-18",
+                "day": 18,
+                "ariaDay": "Friday 18th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-19",
-                today: false,
-                day: 19,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-19",
+                "day": 19,
+                "ariaDay": "Saturday 19th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-20",
-                today: false,
-                day: 20,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-20",
+                "day": 20,
+                "ariaDay": "Sunday 20th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               }
             ]
           },
           {
-            days: [
+            "days": [
               {
-                date: "2015-12-21",
-                today: false,
-                day: 21,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-21",
+                "day": 21,
+                "ariaDay": "Monday 21st December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-22",
-                today: false,
-                day: 22,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-22",
+                "day": 22,
+                "ariaDay": "Tuesday 22nd December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-23",
-                today: false,
-                day: 23,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-23",
+                "day": 23,
+                "ariaDay": "Wednesday 23rd December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-24",
-                today: false,
-                day: 24,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-24",
+                "day": 24,
+                "ariaDay": "Thursday 24th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-25",
-                today: false,
-                day: 25,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-25",
+                "day": 25,
+                "ariaDay": "Friday 25th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-26",
-                today: false,
-                day: 26,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-26",
+                "day": 26,
+                "ariaDay": "Saturday 26th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-27",
-                today: false,
-                day: 27,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-27",
+                "day": 27,
+                "ariaDay": "Sunday 27th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               }
             ]
           },
           {
-            days: [
+            "days": [
               {
-                date: "2015-12-28",
-                today: false,
-                day: 28,
-                available_times_count: 6,
-                bookable: true
+                "date": "2015-12-28",
+                "day": 28,
+                "ariaDay": "Monday 28th December",
+                "today": false,
+                "available_times_count": 6,
+                "bookable": true
               },
               {
-                date: "2015-12-29",
-                today: false,
-                day: 29,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-29",
+                "day": 29,
+                "ariaDay": "Tuesday 29th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-30",
-                today: false,
-                day: 30,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-30",
+                "day": 30,
+                "ariaDay": "Wednesday 30th December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2015-12-31",
-                today: false,
-                day: 31,
-                available_times_count: 0,
-                bookable: true
+                "date": "2015-12-31",
+                "day": 31,
+                "ariaDay": "Thursday 31st December",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": true
               },
               {
-                date: "2016-01-01",
-                today: false,
-                day: 1,
-                available_times_count: 0,
-                bookable: false
+                "date": "2016-01-01",
+                "day": 1,
+                "ariaDay": "Friday 1st January",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": false
               },
               {
-                date: "2016-01-02",
-                today: false,
-                day: 2,
-                available_times_count: 0,
-                bookable: false
+                "date": "2016-01-02",
+                "day": 2,
+                "ariaDay": "Saturday 2nd January",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": false
               },
               {
-                date: "2016-01-03",
-                today: false,
-                day: 3,
-                available_times_count: 0,
-                bookable: false
+                "date": "2016-01-03",
+                "day": 3,
+                "ariaDay": "Sunday 3rd January",
+                "today": false,
+                "available_times_count": 0,
+                "bookable": false
               }
             ]
           }
