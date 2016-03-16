9d8ce74420648c98b470a29890c579f7f449e8a2
Marzena Sokolowska <msokolo@thoughtworks.com>
2016-03-16 16:58:46 +1100

    Order of scripts on pages changed

--- dist/oldJS/calendar_old.js
+++ dist/oldJS/calendar_old.js
@@ -2,4 +2,9 @@
 
 if(!('querySelector' in document && 'localStorage' in window && 'addEventListener' in window)) {
 	document.querySelector('.warning-oldbrowsers').style.display='block';
-}     
+}
+
+if(document.querySelector('.no-available-dates') != null){
+	window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};
+	window.ga('send', 'event', 'Calendar', 'no_appointments');
+}
