3aa64850aa224f5cf57b82d9ac036a002f70cf46
Emily Siow <emily.siow@digital.gov.au>
2016-07-08 11:53:19 +1000

    removes unused sass/css

--- build.js
+++ build.js
@@ -9,22 +9,9 @@ mkdirp.sync(path.join(__dirname, 'dist'));
 
 let result = sass.renderSync({
   file: path.join(__dirname, 'sass', 'bundle.scss'),
-  includePaths: [path.join(__dirname, 'node_modules', 'node-normalize-scss'),
-                 path.join(__dirname, 'node_modules', 'bourbon'),
-                 path.join(__dirname, 'node_modules', 'neat')],
-  excludePaths: [path.join(__dirname, 'sass', 'pages', 'IElte8')],
   outputStyle: 'compressed',
   outFile: 'bundle.css',
   sourceMap: true
 });
 fs.writeFileSync(path.join(__dirname, 'dist', 'bundle.css'), result.css);
 fs.writeFileSync(path.join(__dirname, 'dist', 'bundle.css.map'), result.map);
-
-
-let resultIE8 = sass.renderSync({
-  file: path.join(__dirname, 'sass', 'bundle-IElte8.scss'),
-  outputStyle: 'compressed',
-  outFile: 'bundle.css',
-  sourceMap: false
-});
-fs.writeFileSync(path.join(__dirname, 'dist', 'bundle-IElte8.css'), resultIE8.css);
