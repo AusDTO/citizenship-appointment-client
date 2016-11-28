10aeb230828d6e14191db164581f96961968652c
Yiannis Paschalidis <ypgitgm@gmail.com>
2016-11-28 13:10:56 +1100

    UX changes

--- server.js
+++ server.js
@@ -94,8 +94,7 @@ app.get('/login', (req, res) => {
 app.get('/calendar', (req, res) => {
 
   const time = moment().add(1, 'days');
-  const appointment_date = time.format('dddd D MMMM YYYY');
-  const appointment_time = time.format('h:mm A');
+  const current_appointment = time.format('dddd D MMMM YYYY, h:mm A');
 
   res.render('calendar_page', {
     partials: extendObject(
@@ -108,8 +107,7 @@ app.get('/calendar', (req, res) => {
     error: req.query.error,
     unavailable: req.query.unavailable,
     not_eligible: req.query.not_eligible,
-    appointment_date,
-    appointment_time,
+    current_appointment,
     location,
     locationURL,
     today_date: moment().format('YYYY-MM-DDTHH:mm:ss'),
